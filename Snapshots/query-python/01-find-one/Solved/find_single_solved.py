0a7494d1e57cd3a25ae91549b204a0f4249015b7
Julianna Chen <45462803+jchenj@users.noreply.github.com>
2022-10-05 07:30:05 -0700

    Add query-python files

---
+++ Snapshots/query-python/01-find-one/Solved/find_single_solved.py
@@ -0,0 +1,39 @@
+"""Code for Python drivers course - Unit 2, Lesson 3.
+
+Lesson title: Querying a MongoDB Collection in Python Applications
+
+Learning objectives:
+- Find documents using find() and find_one()
+- Describe how queries are expressed in Python
+"""
+
+import os
+import pprint
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+# Import ObjectId from bson package (part of PyMongo distribution) to enable querying by ObjectId
+from bson.objectid import ObjectId
+
+# Load config from .env file
+load_dotenv()
+MONGODB_URI = os.environ["MONGODB_URI"]
+
+# Connect to MongoDB cluster with MongoClient
+client = MongoClient(MONGODB_URI)
+
+# Get reference to 'bank' database
+db = client.bank
+
+# Get a reference to the 'accounts' collection
+accounts_collection = db.accounts
+
+# Query by ObjectId
+document_to_find = {"_id": ObjectId("62d6e04ecab6d8e1304974ae")}
+
+# TODO: Write an expression that retrieves the document matching the query constraint in the 'accounts' collection. Assign the result of the operation to a variable named 'result'.
+result = accounts_collection.find_one(document_to_find)
+pprint.pprint(result)
+
+client.close()
---
+++ Snapshots/query-python/02-find-many/Unsolved/find_multiple_unsolved.py
@@ -0,0 +1,42 @@
+"""Code for Python drivers course - Unit 2, Lesson 3.
+
+Lesson title: Querying a MongoDB Collection in Python Applications
+
+Learning objectives:
+- Find documents using find() and find_one()
+- Describe how queries are expressed in Python
+"""
+
+import os
+import pprint
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+# Load config from .env file
+load_dotenv()
+MONGODB_URI = os.environ["MONGODB_URI"]
+
+# Connect to MongoDB cluster with MongoClient
+client = MongoClient(MONGODB_URI)
+
+# Get reference to 'bank' database
+db = client.bank
+
+# Get a reference to the 'accounts' collection
+accounts_collection = db.accounts
+
+# Query
+documents_to_find = {"balance": {"$gt": 4700}}
+
+# TODO: Write an expression that selects the documents matching the query constraint in the 'accounts; collection. Assign the result of the operation to a variable named 'cursor'.
+
+
+num_docs = 0
+for document in cursor:
+    num_docs += 1
+    pprint.pprint(document)
+    print()
+print("# of documents found: " + str(num_docs))
+
+client.close()
